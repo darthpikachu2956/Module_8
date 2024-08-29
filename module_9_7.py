e42b0a94293575d2e35b73aa122abdbe201be814
Vlad <darthpikachu2956@gmail.com>
2024-08-29 20:11:57 +0300

    module_9_7.py

--- module_9_7.py
+++ module_9_7.py
@@ -5,14 +5,15 @@ def is_prime(fun):
             return False
         for i in range(2, number):
             if number % i == 0:
-                return 'Составное'
-        return 'Простое'
+                return f'Составное: {number}'
+        return f'Простое: {number}'
     return wrapper
 
 
 @is_prime
 def sum_three(a, b, c):
-    return a + b + c
+    d = a + b + c
+    return d
 
 
 result_1 = sum_three(1, 0, 0)
